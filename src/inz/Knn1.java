698a51082a9b2f8bb5e1afbd687f85868f01aef4
jarex410 <jarex410@gmail.com>
2015-06-10 20:18:43 +0200

    1.44q
    DZialajacy przechodzenie po bazie i generowanie PKT KNN. W czasie testu
    comitowane.

--- src/inz/Knn1.java
+++ src/inz/Knn1.java
@@ -20,15 +20,25 @@ public class Knn1 {
 
 	public static void main(String[] args) throws IOException {
 		String dir = "D:/IN¯YNIERKA/Desc/";
-		String pom = "";
 		String nazwa = "";
 		String nazwa2 = "";
 		String dir2 = "D:/IN¯YNIERKA/Knn/";
 		String pom4 = "";
-		ArrayList<String> lista = new ArrayList<String>();
-		ArrayList<String> lista2 = new ArrayList<String>();
 
-		int liczbaPkt1 = 0, liczbaPkt2 = 0, liczbaPkt = 0; // ZMIENNE DO
+		// ZMIENNE POMOCNICZE DO
+		// PRZECHOWYWANIA DANYCH DO
+		// OBLICZEN
+		String pom5 = "";
+		String pom6 = "";
+		String pom7 = "";
+		String wsp1Max = "";
+		String wsp2Max = "";
+		double suma = 0;
+		double min1 = 100;
+		double min2 = 100;
+		int z = 0;
+
+		 // ZMIENNE DO
 		// PRZECHOWYWANIA
 		// WARTOSCI
 		// ODNALEZIONYCH
@@ -47,371 +57,185 @@ public class Knn1 {
 
 				for (int k = 0; k < 10; k++) {
 
+					ArrayList<String> lista = new ArrayList<String>();
+					ArrayList<String> lista2 = new ArrayList<String>();
+
 					if (i < 10) {
+
 						nazwa2 = "0" + i + "_0" + j + "_0" + k + ".jpg";
-						File plik = new File(dir2 + " KNN  " + nazwa + " TO  "
-								+ nazwa2+".txt");
-						FileWriter zapis = new FileWriter(plik, true);
-						FileReader fr = new FileReader(dir + "Desc " + nazwa
-								+ ".txt");
-						BufferedReader bfr = new BufferedReader(fr);
-						FileReader fr2 = new FileReader(dir + "Desc " + nazwa2
-								+ ".txt");
-						BufferedReader bfr2 = new BufferedReader(fr2);
-
-						while ((pom = bfr.readLine()) != null) { // PIERWSZY
-																	// PLIK
-																	// WCZYTANIE
-																	// DO
-							// LISTY
-							lista.add(pom);
-							for (int ii = 0; ii < 64; ii++) {
-								pom = bfr.readLine();
-								lista.add(pom);
-							}
-							liczbaPkt1++;
-						}
-
-						while ((pom = bfr2.readLine()) != null) { // DRUGI PLIK
-																	// WCZYTANIE
-																	// DO
-							// LISTY
-							lista2.add(pom);
-							for (int ii = 0; ii < 64; ii++) {
-								pom = bfr2.readLine();
-								lista2.add(pom);
-							}
-							liczbaPkt2++;
-						}
-
-						if (liczbaPkt1 > liczbaPkt2) // OBLICZANIE MINIMALNEJ
-														// LICZBY PKT
-							// WLASCIWIIE NIEPOTRZEBNE
-							liczbaPkt = liczbaPkt2;
-						else
-							liczbaPkt = liczbaPkt1;
-
-					// ZMIENNE POMOCNICZE DO
-											// PRZECHOWYWANIA DANYCH DO
-						// OBLICZEN
-						String pom5 = "";
-						String pom6 = "";
-						String pom7 = "";
-						String wsp1Max = "";
-						String wsp2Max = "";
-						double suma = 0;
-						double min1 = 100;
-						double min2 = 100;
-						int z = 0;
-
-						// ZAPIS
-						// WYNIKOW
-						// DO PLIKU
-						Iterator<String> it = lista.iterator(); // DESKRYPTORY
-																// PIERWSZEGO
-						// OBRAZKA
-						Iterator<String> it2 = lista2.iterator();// DESKRYPTORY
-																	// 2 OBRAZKA
-						int skok = 0;
-						double[] tabSum = new double[liczbaPkt1 * liczbaPkt2]; // TABLICA
-						String pomZapas = ""; // PRZECHOWUJACA
-						// WYNIKI // DO
-						// KNN
-						int licznikObrotu = 0; // ZMIENNE POMOCNA PRZY
-												// WYLICZENIU PRZESUNIECIA
-
-						while (it.hasNext() && it2.hasNext()) { // PETELKA
-																// PRZECHODZ¥CA
-																// PO
-							// KOLEKCJACH
-							// pom6 = it.next();
-
-							for (int kk = 0; kk < liczbaPkt2 && kk < liczbaPkt; kk++) // PETLA
-							// KTORA
-							// OBSLUGUJE
-							// SPRAWDZANIE PKTx1 z listy
-							// 1 z PKTx1....xn z listy 2
-
-							{
-								pom6 = it.next(); // WSPOLRZEDNE PKT
-								pom7 = it2.next();
-								// System.out.println(pom6 + " POOOOOM 66");
-								// System.out.println(pom7 + " POOOOOM 77");
-
-								// System.out.println(pom4 + "POM4 PKT");
-								for (int jj = 0; jj < 64; jj++) { // PRZECHODZENIE
-																// PO KOLEKCJACH
-																// W
-									// CELU POBRANIA DANYCH DESC
-									pom4 = it.next();
-									pom5 = it2.next();
-
-									// System.out.println("POM4 = " + pom4 +
-									// "POM5 = "+ pom5);
-									// //OBLICZANIE KNN SASIADOW
-									suma += (Double.parseDouble(pom4) - Double
-											.parseDouble(pom5))
-											* (Double.parseDouble(pom4) - Double
-													.parseDouble(pom5));
-
-								}
-								suma = Math.sqrt(suma);
-
-								if (kk == 0) {
-									min1 = suma;
-									wsp1Max = pom6 + pom7;
-								} else if (kk == 1) {
-									min2 = suma;
-									wsp2Max = pom6 + pom7;
-								}
-								if (suma < 0.8 && suma < min1) {
-									min1 = suma;
-									wsp1Max = pom6 + pom7;
-								} else if (suma < 0.8 && suma < min2) {
-									min2 = suma;
-									wsp2Max = pom6 + pom7;
-								}
-								/*
-								 * if(kk<1 && suma < 0.8) { zapis.write(pom6 +
-								 * pom7 + "\n"); // FORMAT liczbaPar++; pom6 =
-								 * ""; }
-								 */
-								tabSum[z++] = suma; // WRZUCENIE SUMY DO TABLICY
-													// W CELACH
-								// SPRAWDZENIOWYCH
-
-								// System.out.println("SUMA = " +suma);
-								suma = 0;
-								it = lista.iterator();
-								for (int zz = 0; zz < skok; zz++) { // PRZESUWANIE
-																	// ITERATORA
-									// LISTY PIERWSZEJ W CELU
-									// JEGO ODPOWIEDNIGO
-									// UMIEJSCOWIENIA
-									it.next();
-								}
-							}
-							zapis.write(wsp1Max + "\n" + wsp2Max + "\n"); // FORMAT
-							liczbaPar += 2;
-							pom6 = "";
-							// licznikObrotu++;
-							/*
-							 * if (licznikObrotu <= liczbaPkt1) licznikObrotu++;
-							 * else licznikObrotu = licznikObrotu;
-							 */
-							it2 = lista2.iterator(); // ZEROWANIE ITERATOROW
-							it = lista.iterator();
-							skok = 65 * ++licznikObrotu; // WYLICZANIE WARTOSCI
-															// PRZESUNIÊCIA
-							// ITERATORA PIERWSZEJ LISTY
-							for (int zz = 0; zz < skok; zz++) { // PRZESUWANIE
-																// ITERATORA
-																// LISTY
-								// PIERWSZEJ W CELU JEGO
-								// ODPOWIEDNIGO UMIEJSCOWIENIA
-								it.next();
-
-							}
-
-							// it=lista.iterator();
-						}
-						/*
-						 * for (int zz = 0; zz < z; zz++) // PETELKA DO
-						 * WYŒWIETLANIA SUM { System.out.println(tabSum[zz] +
-						 * " Element =" + zz); }
-						 */
-						zapis.write("Liczba Par = " + liczbaPar);
-						zapis.close();
-
-						// in2.close();
-						fr.close();
-						fr2.close();
 
 					} else {
+
 						nazwa2 = i + "_0" + j + "_0" + k + ".jpg";
-						File plik = new File(dir2 + " KNN  " + nazwa + " TO  "
-								+ nazwa2+".txt");
-						FileWriter zapis = new FileWriter(plik, true);
-						FileReader fr = new FileReader(dir + "Desc " + nazwa
-								+ ".txt");
-						BufferedReader bfr = new BufferedReader(fr);
-						FileReader fr2 = new FileReader(dir + "Desc " + nazwa2
-								+ ".txt");
-						BufferedReader bfr2 = new BufferedReader(fr2);
 
+					}
+					File plik = new File(dir2 + " KNN  " + nazwa + " TO  "
+							+ nazwa2 + ".txt");
+					FileWriter zapis = new FileWriter(plik, true);
+					FileReader fr = new FileReader(dir + "Desc " + nazwa
+							+ ".txt");
+					BufferedReader bfr = new BufferedReader(fr);
+					FileReader fr2 = new FileReader(dir + "Desc " + nazwa2
+							+ ".txt");
+					BufferedReader bfr2 = new BufferedReader(fr2);
+					
+					
 
-						while ((pom = bfr.readLine()) != null) { // PIERWSZY
-																	// PLIK
-																	// WCZYTANIE
-																	// DO
-							// LISTY
+				
+					String pom;
+					int liczbaPkt1 = 0, liczbaPkt2 = 0, liczbaPkt = 0;
+					
+					while ((pom = bfr.readLine()) != null) { // PIERWSZY
+																// PLIK
+																// WCZYTANIE
+																// DO
+						// LISTY
+						lista.add(pom);
+						for (int ii = 0; ii < 64; ii++) {
+							pom = bfr.readLine();
 							lista.add(pom);
-							for (int ii = 0; ii < 64; ii++) {
-								pom = bfr.readLine();
-								lista.add(pom);
-							}
-							liczbaPkt1++;
 						}
+						liczbaPkt1++;
+					}
 
-						while ((pom = bfr2.readLine()) != null) { // DRUGI PLIK
-																	// WCZYTANIE
-																	// DO
-							// LISTY
+					while ((pom = bfr2.readLine()) != null) { // DRUGI PLIK
+																// WCZYTANIE
+																// DO
+						// LISTY
+						lista2.add(pom);
+						for (int ii = 0; ii < 64; ii++) {
+							pom = bfr2.readLine();
 							lista2.add(pom);
-							for (int ii = 0; ii < 64; ii++) {
-								pom = bfr2.readLine();
-								lista2.add(pom);
-							}
-							liczbaPkt2++;
 						}
+						liczbaPkt2++;
+					}
 
-						if (liczbaPkt1 > liczbaPkt2) // OBLICZANIE MINIMALNEJ
-														// LICZBY PKT
-							// WLASCIWIIE NIEPOTRZEBNE
-							liczbaPkt = liczbaPkt2;
-						else
-							liczbaPkt = liczbaPkt1;
+					if (liczbaPkt1 > liczbaPkt2) // OBLICZANIE MINIMALNEJ
+													// LICZBY PKT
+						// WLASCIWIIE NIEPOTRZEBNE
+						liczbaPkt = liczbaPkt2;
+					else
+						liczbaPkt = liczbaPkt1;
 
-											// PRZECHOWYWANIA DANYCH DO
-						// OBLICZEN
-						String pom5 = "";
-						String pom6 = "";
-						String pom7 = "";
-						String wsp1Max = "";
-						String wsp2Max = "";
-						double suma = 0;
-						double min1 = 100;
-						double min2 = 100;
-						int z = 0;
+					Iterator<String> it = lista.iterator(); // DESKRYPTORY// PIERWSZEGO// OBRAZKA											
+					Iterator<String> it2 = lista2.iterator();// DESKRYPTORY
+																// 2 OBRAZKA
+					int skok = 0;
+					int licznikObrotu = 0; // ZMIENNE POMOCNA PRZY
+											// WYLICZENIU PRZESUNIECIA
 
-						// ZAPIS
-						// WYNIKOW
-						// DO PLIKU
-						Iterator<String> it = lista.iterator(); // DESKRYPTORY
-																// PIERWSZEGO
-						// OBRAZKA
-						Iterator<String> it2 = lista2.iterator();// DESKRYPTORY
-																	// 2 OBRAZKA
-						int skok = 0;
-						double[] tabSum = new double[liczbaPkt1 * liczbaPkt2]; // TABLICA
-						String pomZapas = ""; // PRZECHOWUJACA
-						// WYNIKI // DO
-						// KNN
-						int licznikObrotu = 0; // ZMIENNE POMOCNA PRZY
-												// WYLICZENIU PRZESUNIECIA
+					while (it.hasNext() && it2.hasNext()) { // PETELKA
+															// PRZECHODZ¥CA
+															// PO
+						// KOLEKCJACH
+						// pom6 = it.next();
 
-						while (it.hasNext() && it2.hasNext()) { // PETELKA
-																// PRZECHODZ¥CA
+						for (int kk = 0; kk < liczbaPkt2 && kk < liczbaPkt; kk++) // PETLA
+						// KTORA
+						// OBSLUGUJE
+						// SPRAWDZANIE PKTx1 z listy
+						// 1 z PKTx1....xn z listy 2
+
+						{
+							pom6 = it.next(); // WSPOLRZEDNE PKT
+							pom7 = it2.next();
+							// System.out.println(pom6 + " POOOOOM 66");
+							// System.out.println(pom7 + " POOOOOM 77");
+
+							// System.out.println(pom4 + "POM4 PKT");
+							for (int jj = 0; jj < 64; jj++) { // PRZECHODZENIE
 																// PO
-							// KOLEKCJACH
-							// pom6 = it.next();
-
-							for (int kk = 0; kk < liczbaPkt2 && kk < liczbaPkt; kk++) // PETLA
-							// KTORA
-							// OBSLUGUJE
-							// SPRAWDZANIE PKTx1 z listy
-							// 1 z PKTx1....xn z listy 2
-
-							{
-								pom6 = it.next(); // WSPOLRZEDNE PKT
-								pom7 = it2.next();
-								// System.out.println(pom6 + " POOOOOM 66");
-								// System.out.println(pom7 + " POOOOOM 77");
-
-								// System.out.println(pom4 + "POM4 PKT");
-								for (int jj = 0; jj < 64; jj++) { // PRZECHODZENIE
-																// PO KOLEKCJACH
+																// KOLEKCJACH
 																// W
-									// CELU POBRANIA DANYCH DESC
-									pom4 = it.next();
-									pom5 = it2.next();
+								// CELU POBRANIA DANYCH DESC
+								pom4 = it.next();
+								pom5 = it2.next();
 
-									// System.out.println("POM4 = " + pom4 +
-									// "POM5 = "+ pom5);
-									// //OBLICZANIE KNN SASIADOW
-									suma += (Double.parseDouble(pom4) - Double
-											.parseDouble(pom5))
-											* (Double.parseDouble(pom4) - Double
-													.parseDouble(pom5));
+								// System.out.println("POM4 = " + pom4 +
+								// "POM5 = "+ pom5);
+								// //OBLICZANIE KNN SASIADOW
+								suma += (Double.parseDouble(pom4) - Double
+										.parseDouble(pom5))
+										* (Double.parseDouble(pom4) - Double
+												.parseDouble(pom5));
 
-								}
-								suma = Math.sqrt(suma);
-
-								if (kk == 0) {
-									min1 = suma;
-									wsp1Max = pom6 + pom7;
-								} else if (kk == 1) {
-									min2 = suma;
-									wsp2Max = pom6 + pom7;
-								}
-								if (suma < 0.8 && suma < min1) {
-									min1 = suma;
-									wsp1Max = pom6 + pom7;
-								} else if (suma < 0.8 && suma < min2) {
-									min2 = suma;
-									wsp2Max = pom6 + pom7;
-								}
-								/*
-								 * if(kk<1 && suma < 0.8) { zapis.write(pom6 +
-								 * pom7 + "\n"); // FORMAT liczbaPar++; pom6 =
-								 * ""; }
-								 */
-								tabSum[z++] = suma; // WRZUCENIE SUMY DO TABLICY
-													// W CELACH
-								// SPRAWDZENIOWYCH
-
-								// System.out.println("SUMA = " +suma);
-								suma = 0;
-								it = lista.iterator();
-								for (int zz = 0; zz < skok; zz++) { // PRZESUWANIE
-																	// ITERATORA
-									// LISTY PIERWSZEJ W CELU
-									// JEGO ODPOWIEDNIGO
-									// UMIEJSCOWIENIA
-									it.next();
-								}
 							}
-							zapis.write(wsp1Max + "\n" + wsp2Max + "\n"); // FORMAT
-							liczbaPar += 2;
-							pom6 = "";
-							// licznikObrotu++;
+							suma = Math.sqrt(suma);
+
+							if (kk == 0) {
+								min1 = suma;
+								wsp1Max = pom6 + pom7;
+							} else if (kk == 1) {
+								min2 = suma;
+								wsp2Max = pom6 + pom7;
+							}
+							if (suma < 0.8 && suma < min1) {
+								min1 = suma;
+								wsp1Max = pom6 + pom7;
+							} else if (suma < 0.8 && suma < min2) {
+								min2 = suma;
+								wsp2Max = pom6 + pom7;
+							}
 							/*
-							 * if (licznikObrotu <= liczbaPkt1) licznikObrotu++;
-							 * else licznikObrotu = licznikObrotu;
+							 * if(kk<1 && suma < 0.8) { zapis.write(pom6 + pom7
+							 * + "\n"); // FORMAT liczbaPar++; pom6 = ""; }
 							 */
-							it2 = lista2.iterator(); // ZEROWANIE ITERATOROW
+							// tabSum[z++] = suma; // WRZUCENIE SUMY DO
+							// TABLICY
+							// W CELACH
+							// SPRAWDZENIOWYCH
+
+							// System.out.println("SUMA = " +suma);
+							suma = 0;
 							it = lista.iterator();
-							skok = 65 * ++licznikObrotu; // WYLICZANIE WARTOSCI
-															// PRZESUNIÊCIA
-							// ITERATORA PIERWSZEJ LISTY
 							for (int zz = 0; zz < skok; zz++) { // PRZESUWANIE
 																// ITERATORA
-																// LISTY
-								// PIERWSZEJ W CELU JEGO
-								// ODPOWIEDNIGO UMIEJSCOWIENIA
+								// LISTY PIERWSZEJ W CELU
+								// JEGO ODPOWIEDNIGO
+								// UMIEJSCOWIENIA
 								it.next();
-
 							}
-
-							// it=lista.iterator();
 						}
+						zapis.write(wsp1Max + "\n" + wsp2Max + "\n"); // FORMAT
+						liczbaPar += 2;
+						pom6 = "";
+						// licznikObrotu++;
 						/*
-						 * for (int zz = 0; zz < z; zz++) // PETELKA DO
-						 * WYŒWIETLANIA SUM { System.out.println(tabSum[zz] +
-						 * " Element =" + zz); }
+						 * if (licznikObrotu <= liczbaPkt1) licznikObrotu++;
+						 * else licznikObrotu = licznikObrotu;
 						 */
-						zapis.write("Liczba Par = " + liczbaPar);
-						zapis.close();
+						it2 = lista2.iterator(); // ZEROWANIE ITERATOROW
+						it = lista.iterator();
+						skok = 65 * ++licznikObrotu; // WYLICZANIE WARTOSCI
+														// PRZESUNIÊCIA
+						// ITERATORA PIERWSZEJ LISTY
+						for (int zz = 0; zz < skok; zz++) { // PRZESUWANIE
+															// ITERATORA
+															// LISTY
+							// PIERWSZEJ W CELU JEGO
+							// ODPOWIEDNIGO UMIEJSCOWIENIA
+							it.next();
 
-						// in2.close();
-						fr.close();
-						fr2.close();
+						}
+
+						// it=lista.iterator();
 					}
+					/*
+					 * for (int zz = 0; zz < z; zz++) // PETELKA DO WYŒWIETLANIA
+					 * SUM { System.out.println(tabSum[zz] + " Element =" + zz);
+					 * }
+					 */
+					zapis.write("Liczba Par = " + liczbaPar);
+					liczbaPar=0;
+					zapis.close();
+
+					// in2.close();
+					fr.close();
+					fr2.close();
 
 				}
 
 			}
-
 
 		}
 
